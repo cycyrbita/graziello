2ef8b2773a485564ebe3d1bf91a638a975e244a0
Nikita job <cycyrbita@mail.ru>
2021-04-14 18:40:30 +0300

    шпка готова

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,32 @@ $(document).ready(function () {
         }
     })
 
+    var lastScrollTop = 0;
+    $(window).scroll(function () {
+        var $header = $('.header').outerHeight();
+        var st = $(this).scrollTop();
+        if (st < lastScrollTop && st >= 0) {
+            $('body').css('padding-top', $header);
+            $('.header').addClass('mobile');
+        } else {
+            $('body').css('padding-top', '0');
+            $('.header').removeClass('mobile');
+        }
+        lastScrollTop = st;
+    });
+
+    $(document).mouseup(function (e) {
+		var div = $('.nav__burger'),
+		    div2 = $('.nav__list');
+		if (!div.is(e.target) && div.has(e.target).length === 0 && !div2.is(e.target) && div2.has(e.target).length === 0) {
+			$('.nav').removeClass('visible');
+		}
+	});
+
+    $('.nav__burger').click(function() {
+        $('.nav').toggleClass('visible');
+    })
+
     $('.js-commits__box').owlCarousel({
         loop: true,
         autoHeight: true,
